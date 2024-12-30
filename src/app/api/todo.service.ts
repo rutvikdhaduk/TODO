4e442c9ac61027602116f70189cab12571469c83
rutvik dhaduk <rutvikdhaduk5@gmail.com>
2024-12-30 23:50:44 +0530

    api's write and some design add

---
+++ src/app/api/todo.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  url = 'http://localhost:8080'
+  constructor(public http:HttpClient) { }
+
+  addTodo(body) {
+    this.http.post(`${this.url}`,body)
+  }
+
+  updateTodo(id,body) {
+    this.http.put(`${this.url}/${id}`,body)
+  }
+
+  deleteTodo(id) {
+    this.http.delete(`${this.url}/${id}`)
+  }
+
+}
+
+export class Todo {
+  name: string;
+  title: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  isCompleted: boolean;
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TodoAddComponent } from './todo-add/todo-add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'task',
+    pathMatch:'full'
+  },
+  {
+    path:'task',
+    component:TodoAddComponent
+  }
+];
